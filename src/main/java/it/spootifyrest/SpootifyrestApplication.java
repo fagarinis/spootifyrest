a8d4273131dbe5532b1e73989fea9508b8ab8724
federicof1994 <47894627+federicof1994@users.noreply.github.com>
2020-03-09 19:40:08 +0100

    primo commit

---
+++ src/main/java/it/spootifyrest/SpootifyrestApplication.java
@@ -0,0 +1,13 @@
+package it.spootifyrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpootifyrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpootifyrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/spootifyrest/SpootifyrestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.spootifyrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpootifyrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
